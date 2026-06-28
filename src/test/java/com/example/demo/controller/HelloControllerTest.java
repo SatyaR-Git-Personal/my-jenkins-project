b10498fab1868b1384f25248a392cf94ce8d2ff6
routs <satyar.codes@gmail.com>
2026-06-28 12:20:45 +0530

    Initial Java project

---
+++ src/test/java/com/example/demo/controller/HelloControllerTest.java
@@ -0,0 +1,22 @@
+package com.example.demo.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HelloControllerTest {
+
+   HelloController controller= new HelloController();
+
+   @Test
+   void testHello() {
+      String response= controller.hello();
+      assertEquals("Hello from Spring Boot Jenkins Demo", response);
+   }
+
+   @Test
+   void testStatus() {
+      String response= controller.status();
+      assertEquals("Application is running", response);
+   }
+}
